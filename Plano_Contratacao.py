2dbed842fb0d4c1f6ddde31863ccaeac5075b8fd
gibransodre <gibransodre@gmail.com>
2025-06-17 18:11:58 -0300

    Correção de erros na classe Plano_Contratacao

--- Plano_Contratacao.py
+++ Plano_Contratacao.py
@@ -1,12 +1,12 @@
-import integracao
+import Integracao
 import json 
 import os
-import conexao
-import buscar_json
+import Conexao
+import Buscar_json
 from datetime import datetime
 
 json_pca = None
-token = conexao.get_token()
+token = Conexao.get_token()
 files = None
 
 cnpj = os.getenv("cnpj_treinamento")
@@ -18,7 +18,7 @@ def inserirPlanoContratacao():
     ano = input("Informe o ano do PCA")
     endpoint = f"/v1/orgaos/{cnpj}/pca"
     url_json = f"https://raw.githubusercontent.com/{usuario_git}/{repositorio}/refs/heads/main/Arquivos_Json/InserirPCA.json"
-    json_pca = buscar_json.buscar_json_raw(url_json)
+    json_pca = Buscar_json.buscar_json_raw(url_json)
     #data_hora = datetime.now()
     json_pca["anoPca"] = ano
     json_pca["itensPlano"][0]["dataDesejada"] = f"{ano}-07-07"
@@ -27,5 +27,5 @@ def inserirPlanoContratacao():
         "Accept": "*/*",
         "Content-Type": "application/json"
         }    
-    response = integracao.executa_endpoint(endpoint, json.dumps(json_pca, indent =4), headers, files, False)
+    response = Integracao.executa_endpoint(endpoint, json.dumps(json_pca, indent =4), headers, files, False)
     return response 
